29e2f7308f291061de5069603364bcb17fa64a1f
Tenari the Great <djz24@case.edu>
2013-03-21 09:49:22 -0400

    Master Start
    Not much works

---
+++ Assets/Scripts/EnemyUnitAIMovementController.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using System.Collections;
+
+public class EnemyUnitAIMovementController : MonoBehaviour {
+	
+	private CharacterMotor motor;
+	private Vector3 directionVector;
+	
+	// Use this for initialization
+	void Start () {
+		motor = GetComponent<CharacterMotor>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Get the input vector from AI calculations
+		directionVector = nextDirectionVector();
+	
+		if (directionVector != Vector3.zero) {
+			// Get the length of the directon vector and then normalize it
+			// Dividing by the length is cheaper than normalizing when we already have the length anyway
+			float directionLength = directionVector.magnitude;
+			directionVector = directionVector / directionLength;
+			
+			// Make sure the length is no bigger than 1
+			directionLength = Mathf.Min(1, directionLength);
+			
+			// Make the input vector more sensitive towards the extremes and less sensitive in the middle
+			// This makes it easier to control slow speeds when using analog sticks
+			directionLength = directionLength * directionLength;
+			
+			// Multiply the normalized direction vector by the modified length
+			directionVector = directionVector * directionLength;
+		}
+		
+		// Apply the direction to the CharacterMotor
+		motor.inputMoveDirection = transform.rotation * directionVector;
+	}
+	
+	Vector3 nextDirectionVector(){
+		return new Vector3(10,0,0);
+	}
+}
---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+using System.Collections;
+
+public class Health : MonoBehaviour {
+	/// <summary>
+	/// The primary variables of the Health functionality.
+	/// </summary>
+	public int health = 100;					// The actual current health of the entity
+	public bool dead = false;					// The death? condition. If true, entity is dead.
+	
+	/// <summary>
+	/// The other abstraction variables.
+	/// </summary>
+	public int startHealth = 100;				//The amount of health the entity starts with.
+	public bool startDead = false;				//The default death status of the entity.
+	
+	
+	// Use this for initialization
+	void Start () {
+		dead = startDead;						// Initializes the 'dead' status of the entity
+		health = startHealth;					// Initializes the health of the entity
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (health <= 0){						// If the health falls below Magic Number '0'...
+			Died();								// ... then the entity died. Death is handled by Died()
+		}
+	}
+	
+	/// <summary>
+	/// Takes the damage.
+	/// </summary>
+	/// <param name='dmgAmount'>
+	/// Dmg amount.
+	/// </param>
+	void TakeDamage(int dmgAmount){
+		health -= dmgAmount;
+	}
+	
+	/// <summary>
+	/// Gains the health.
+	/// </summary>
+	/// <param name='healAmount'>
+	/// Heal amount.
+	/// </param>
+	void GainHealth(int healAmount){
+		health += healAmount;
+	}
+	
+	/// <summary>
+	/// Died this instance.
+	/// </summary>
+	void Died(){
+		dead = true;
+	}
+}
