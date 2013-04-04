0ed218e60c24edd9a935491f1e8f5c4489fcc4e4
Tenari the Great <djz24@case.edu>
2013-04-04 00:04:36 -0400

    Made the player able to spawn units by clicking on a house.

---
+++ Assets/Scripts/SpawnAllyUnit.cs
@@ -0,0 +1,36 @@
+// Daniel Zapata
+using UnityEngine;
+using System.Collections;
+
+public class SpawnAllyUnit : MonoBehaviour {
+	
+	public string allyUnitPrefabName = "YourUnit";// Prefab name.
+	public Vector3 pos;					// The position of the spawn.
+	public float houseSize = 20.0F;		// The size of the houses.
+	
+	public int numUnitsLeft = 4;		// Limits the number of units able to be generated.
+	// Use this for initialization
+	void Start () {
+		pos = new Vector3(gameObject.transform.position.x+houseSize,
+						  gameObject.transform.position.y,
+						  gameObject.transform.position.z+houseSize);
+	}
+	
+	// Update is called once per frame
+	// Regens units Left randomly over time.
+	void Update () {
+		int rand = (int)Random.Range(0,1000);
+		if (rand >= 999){
+			numUnitsLeft++;
+		}
+	}
+	
+	// Is called to spawn a new unit.
+	public void SpawnNewUnit(){
+		if(numUnitsLeft>0){			// If units remain in the house,
+			numUnitsLeft--;			// Subtract one, and send him out.
+			// Instantiate a copy of the unit at the initially determined position, with this rotation.
+			Instantiate(Resources.Load(allyUnitPrefabName,typeof(GameObject)), pos, transform.rotation);
+		}
+	}
+}
---
+++ Assets/Scripts/TellHouseToSpawn.cs
@@ -0,0 +1,42 @@
+// Daniel Zapata
+using UnityEngine;
+using System.Collections;
+
+// Put inside of Main player to allow them to spawn units from houses.
+public class TellHouseToSpawn : MonoBehaviour {
+	
+	public float rayCastDistance = 500.0F;
+	
+	public string house1Tag = "House1";
+	public string house2Tag = "House2";
+	public string house3Tag = "House3";
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	// In this implementation, it tries to send a spawnunit message to houses in front of it.
+	void Update () {
+		
+		// If the left click is down,
+		if(Input.GetMouseButtonDown(0)){
+			
+			RaycastHit hit;			// Make the thing to receive information on hits.
+			// If the Raycast from the cam's position, along the cam's direction hits something within rayCastDistance meters, it'll put collision info in hit.
+			if(Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, rayCastDistance)){
+				
+				// If we hit a house.
+				if ( (hit.collider.CompareTag(house1Tag)) || (hit.collider.CompareTag(house2Tag)) || (hit.collider.CompareTag(house3Tag))){
+					
+					// Spawn a new unit.
+					hit.collider.GetComponent<SpawnAllyUnit>().SendMessage("SpawnNewUnit");
+				}
+			}
+		}
+	
+	}
+	
+	
+}
